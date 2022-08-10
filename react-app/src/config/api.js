5594c89a38393875786f7321348476f0d3d13315
Yash Brahmkshatriya <yash311000@gmail.com>
2022-08-10 11:24:26 +0530

    [UPDATE] api change

--- react-app/src/config/api.js
+++ react-app/src/config/api.js
@@ -1,3 +1,3 @@
-export const hostNameUrl = "http://localhost:8080/api/v1";
-// export const hostNameUrl =
-//   "https://db-grads-9z8v-group-15.nw.r.appspot.com/api/v1";
+// export const hostNameUrl = "http://localhost:8080/api/v1";
+export const hostNameUrl =
+  "https://db-grads-9z8v-group-15.nw.r.appspot.com/api/v1";
